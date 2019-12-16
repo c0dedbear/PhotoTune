276159f170d952325db2b70456d9df07aa3b48a2
Anna Belousova <anna.belousova.2011@gmail.com>
2019-12-16 21:35:28 +0300

    Изменила файл ViewController

---
+++ PhotoTune/Modules/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PhotoTune
+//
+//  Created by Mikhail Medvedev on 16.12.2019.
+//  Copyright © 2019 Mikhail Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+	   super.viewDidLoad()
+   }
+}
