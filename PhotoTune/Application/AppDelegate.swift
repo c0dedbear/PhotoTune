1b2d0046530a174887919b03a3f0e2000faa8f18
Mikhail Medvedev <icodedbear@gmail.com>
2019-12-16 20:46:39 +0300

    Basic project config

---
+++ PhotoTune/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  PhotoTune
+//
+//  Created by Mikhail Medvedev on 16.12.2019.
+//  Copyright © 2019 Mikhail Medvedev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate
+{
+
+	var window: UIWindow?
+
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+		return true
+	}
+}
