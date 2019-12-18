f612f83b819f2dd4b56d290464234b7a29429859
Mikhail Medvedev <icodedbear@gmail.com>
2019-12-18 13:27:36 +0300

    (SAM-7) Добавил в проект модуль EditingScreen с базовой настройкой

---
+++ PhotoTune/Modules/EditingScreen/Views/FiltersCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  FiltersCollectionView.swift
+//  PhotoTune
+//
+//  Created by Mikhail Medvedev on 18.12.2019.
+//  Copyright © 2019 Mikhail Medvedev. All rights reserved.
+//
+
+import UIKit
+
+final class FiltersCollectionView: UICollectionView
+{
+
+}
