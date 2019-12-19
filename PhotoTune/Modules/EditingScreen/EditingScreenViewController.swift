aca62687d213fab99a38097d02100ed47cc928ed
c0dedbear <icodedbear@gmail.com>
2019-12-19 17:04:46 +0300

    (SAM-13)  Добавил BarButton'ы

--- PhotoTune/Modules/EditingScreen/EditingScreenViewController.swift
+++ PhotoTune/Modules/EditingScreen/EditingScreenViewController.swift
@@ -19,6 +19,7 @@ protocol IEditingScreen
 
 final class EditingScreenViewController: UIViewController
 {
+	//MARK: Properties
 	private let presenter: IEditingScreenPresenter
 
 	private var imageView = UIImageView()
@@ -35,12 +36,43 @@ final class EditingScreenViewController: UIViewController
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-
+	//MARK: VC Life Cycle Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setupNavigationBar()
+		setupToolBar()
+	}
+	//MARK: Private Methods
+	private func setupNavigationBar() {
+		navigationItem.leftBarButtonItem = UIBarButtonItem(
+			barButtonSystemItem: .cancel,
+			target: self,
+			action: #selector(cancelTapped))
+
+		navigationItem.rightBarButtonItem = UIBarButtonItem(
+			barButtonSystemItem: .done,
+			target: self,
+			action: #selector(doneTapped))
+	}
+
+	private func setupToolBar() {
+		let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+		let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+		toolbarItems = [spacer, add, spacer, add, spacer, add, spacer]
+		navigationController?.toolbar.tintColor = .black
+		navigationController?.setToolbarHidden(false, animated: false)
+	}
+
+	@objc private func cancelTapped() {
+		//dismiss VC
+	}
+
+	@objc private func doneTapped() {
+		//save edited image
 	}
 }
 
+	//MARK: - IEditingScreen
 extension EditingScreenViewController: IEditingScreen
 {
 	func rotateImage() {
