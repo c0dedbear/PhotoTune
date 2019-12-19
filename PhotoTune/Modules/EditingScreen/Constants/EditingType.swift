b83a22d16b19486d226738942d8c65fff8e59130
c0dedbear <icodedbear@gmail.com>
2019-12-19 17:20:40 +0300

    SAM-13, Добавил енум для типа редактирования

---
+++ PhotoTune/Modules/EditingScreen/Constants/EditingType.swift
@@ -0,0 +1,16 @@
+//
+//  EditingType.swift
+//  PhotoTune
+//
+//  Created by Mikhail Medvedev on 19.12.2019.
+//  Copyright © 2019 Mikhail Medvedev. All rights reserved.
+//
+
+import Foundation
+
+enum EditingType: String
+{
+	case filters = "Filters"
+	case tune = "Tune"
+	case rotation = "Rotation"
+}
