c40e3e3928c2c659091a788bdc858289be1c978f
Саша Руцман <rutsman@bk.ru>
2020-01-10 12:38:10 +0300

    Исправил свифтлинт

--- PhotoTune/BusinessLayer/CollectionView/PhotoCollectionViewCell.swift
+++ PhotoTune/BusinessLayer/CollectionView/PhotoCollectionViewCell.swift
@@ -5,23 +5,25 @@
 //  Created by Саша Руцман on 10.01.2020.
 //  Copyright © 2020 Mikhail Medvedev. All rights reserved.
 //
+// swiftlint:disable required_final
 
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell
 {
 	let imageView = UIImageView()
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		addSubview(imageView)
 		makeConstraintsForImageView()
 	}
-	
+
 	@available(*, unavailable)
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	private func makeConstraintsForImageView() {
 		imageView.translatesAutoresizingMaskIntoConstraints = false
 		imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
