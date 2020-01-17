6c2c32e25fb9055af2f79def74709f9389e88350
Anna Belousova <anna.belousova.2011@gmail.com>
2020-01-17 14:47:25 +0300

    SAM-54, Отредактированное фото теперь отображается первым в коллекции

--- PhotoTune/Modules/EditingScreen/EditingScreenPresenter.swift
+++ PhotoTune/Modules/EditingScreen/EditingScreenPresenter.swift
@@ -101,7 +101,7 @@ private extension EditingScreenPresenter
 		storageService.storeImage(currentImage, filename: filename) { [weak self] in
 			self?.storageService.storeImage(previewImage, filename: previewFileName) {
 				if var existingEditedImages = self?.storageService.loadEditedImages() {
-					existingEditedImages.append(editedImage)
+					existingEditedImages.insert(editedImage, at: 0)
 					self?.storageService.saveEditedImages(existingEditedImages)
 				}
 				else {
@@ -133,7 +133,7 @@ private extension EditingScreenPresenter
 				for (index, item) in currentEditedImages.enumerated()
 					where item.imageFileName == editedImage.imageFileName {
 						currentEditedImages.remove(at: index)
-						currentEditedImages.insert(editedImage, at: index)
+						currentEditedImages.insert(editedImage, at: 0)
 				}
 				self?.storageService.saveEditedImages(currentEditedImages)
 				self?.editingScreen?.dismiss(toRoot: true, completion: nil)
